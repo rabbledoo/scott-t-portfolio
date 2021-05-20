3123a97c884523ef4bba069b8da8ca93281438ba
Scott Tarnowsky <scott.tarnowsky@gmail.com>
2021-05-20 16:32:50 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Nav } from "react-bootstrap";
-import './App.css';
-import Navbar from './components/Navbar'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header-wrapper">
+                <div className="main-info">
+                    <h1>web development and website promotion</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
