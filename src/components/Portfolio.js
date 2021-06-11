c59122ba990ba961a575282c61da10820cf67264
Scott Tarnowsky <scott.tarnowsky@gmail.com>
2021-06-11 15:20:40 -0400

    fixed visual bugs

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,23 +5,21 @@ import { Card, Button } from "react-bootstrap";
 function Portfolio() {
   return (
     <>
-    <div className="projectContainer">
-      {Projects.map((items) => {
-        return (
-          
-          <Card style={{ width: "18rem" , display: "flex", }}>
-            <Card.Img variant="top" src={items.image} />
-            <Card.Body>
-              <Card.Title>{items.title}</Card.Title>
-              <Card.Text>{items.text}</Card.Text>
-              <Button onClick={items.click} variant="success">
-                See Live
-              </Button>
-            </Card.Body>
-          </Card>
-        );
-      })}
-     
+      <div className="projectContainer">
+        {Projects.map((items) => {
+          return (
+            <Card style={{ width: "18rem", display: "flex" }}>
+              <Card.Img variant="top" src={items.image} />
+              <Card.Body>
+                <Card.Title>{items.title}</Card.Title>
+                <Card.Text>{items.text}</Card.Text>
+                <Button onClick={items.click} variant="success">
+                  See Live
+                </Button>
+              </Card.Body>
+            </Card>
+          );
+        })}
       </div>
     </>
   );
